254d70093df753c6da1feab743b4f5af0fc34b15
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-03 20:03:19 +0500

    📦 NEW: added first route

--- index.ts
+++ index.ts
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', async (req: express.Request, res) => {
+  res.status(200).send('Welcome to first route.');
+});
 
 // Connection
 const PORT = process.env.PORT || 3000;
