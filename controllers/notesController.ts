158b69b89613c603c3e0c3b415c54065e67cff4f
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-04-04 01:54:16 +0500

    👌 IMPROVE: added get specific note route

--- controllers/notesController.ts
+++ controllers/notesController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import Note from '../models/Note';
 
+// GET fetch all notes for a user
 const getAllNotes = async (req: Request, res: Response) => {
   try {
     const notes = await Note.find({ is_deleted: false });
 
-    if (notes.length === 0) return res.status(404).json({ message: 'No notes found.' });
+    if (notes.length === 0)
+      return res.status(404).json({ message: 'No notes found for this user.' });
 
     res.status(200).json({ notes: notes });
   } catch (error) {
@@ -13,7 +15,19 @@ const getAllNotes = async (req: Request, res: Response) => {
   }
 };
 
-const getSpecificNote = (req: Request, res: Response) => {};
+const getSpecificNote = async (req: Request, res: Response) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note)
+      return res.status(404).json({ message: 'Note with given ID was not found.' });
+
+    res.status(200).json({ note: note });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const createNote = (req: Request, res: Response) => {};
 const updateNote = (req: Request, res: Response) => {};
 const deleteNote = (req: Request, res: Response) => {};
